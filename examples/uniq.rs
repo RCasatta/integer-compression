45466341d804c3be60d26f61b2b7d2f2ca28f80a
Riccardo Casatta <riccardo@casatta.it>
2018-05-07 10:18:17 +0200

    uniq by hash

--- examples/uniq.rs
+++ examples/uniq.rs
@@ -1,5 +1,11 @@
+extern crate sha2;
+extern crate data_encoding;
+
 use std::io;
 use std::collections::HashSet;
+use sha2::{Sha256, Digest};
+use data_encoding::HEXLOWER;
+
 
 fn main() {
     let mut uniq = HashSet::new();
@@ -11,8 +17,23 @@ fn main() {
                 if n == 0 {
                     break;
                 }
-                //println!("{}",buffer);
-                uniq.insert(buffer);
+                //println!("({})",buffer.trim());
+                let bytes = buffer.into_bytes();
+                match HEXLOWER.decode(&bytes[0..bytes.len()-1]) {
+                    Ok(vec) => {
+                        let mut hasher = Sha256::default();
+                        hasher.input(&vec[..]);
+                        let sha_result = hasher.result();
+                        let trunc = &sha_result[0..4];
+                        //println!("({:?})",trunc);
+                        uniq.insert(trunc.to_owned());
+                    },
+                    Err(_) => {
+                        println!("non parsable");
+                    }
+                }
+
+
             }
             Err(error) => panic!("error: {}", error),
         }
